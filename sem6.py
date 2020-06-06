80cc3374de4244895350db46851328632212b8b0
Есенин Е.Е <s02160058@practicum.cs.msu.su>
2020-06-06 13:04:47 +0300

    initial interface

---
+++ sem6.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from tkinter import filedialog as fd
+import subprocess
+ 
+def insertText():
+    pass
+ 
+def extractText():
+    pass
+def onlyExtractText():
+    pass   
+ 
+root = Tk()
+text = Text(width = 100, height = 50)
+text.grid(columnspan = 3)
+b1 = Button(text = "open", command = insertText)
+b1.grid(row = 1, sticky = E)
+b2 = Button(text = "save as", command = extractText)
+b2.grid(row = 1, column=2, sticky = W)
+b3 = Button(text = "save", command = onlyExtractText)
+b3.grid(row = 1, column = 1, sticky = S)
+ 
+root.mainloop()
