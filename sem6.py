8bbe8cae06fb7e9e002633ee92feb58d3744fb36
Есенин Е.Е <s02160058@practicum.cs.msu.su>
2020-06-06 14:33:51 +0300

    basic task+ save as button

--- sem6.py
+++ sem6.py
@@ -1,17 +1,39 @@
 from tkinter import *
 from tkinter import filedialog as fd
+from tkinter import messagebox as mb
 import subprocess
- 
+
 def insertText():
-    file_name = fd.askopenfilename()
-    result = subprocess.run(["xxd", "-g1", file_name], stdout=subprocess.PIPE)
-    text.insert(1.0    , result.stdout)
+    try:
+        global file_name 
+        file_name = fd.askopenfilename()
+        l1.configure(text = str(file_name))
+        text.delete(1.0, END)
+        result = subprocess.run(["xxd", "-g1", file_name], stdout=subprocess.PIPE)
+        text.insert(1.0, result.stdout)
+    except:
+        mb.showerror("Error", "Something went wrong with file opening")  
  
 def extractText():
-    pass
+    try:
+        file_name = fd.asksaveasfilename()
+        s = text.get(1.0, END)
+        result = subprocess.run(["xxd", "-r","-", file_name], input = s.encode())  
+    except:
+        mb.showerror("Error", "Something went wrong with saving")              
+
 def onlyExtractText():
-    pass   
- 
+    try:
+        global file_name
+        s = text.get(1.0, END)
+        l1.configure(text = "File:")
+        result = subprocess.run(["xxd", "-r","-", file_name], input = s.encode())  
+        text.delete(1.0, END)
+    except:
+        mb.showerror("Error", "Something went wrong with saving")  
+    
+
+file_name = "" 
 root = Tk()
 text = Text(width = 100, height = 50)
 text.grid(columnspan = 3)
@@ -21,5 +43,6 @@ b2 = Button(text = "save as", command = extractText)
 b2.grid(row = 1, column=2, sticky = W)
 b3 = Button(text = "save", command = onlyExtractText)
 b3.grid(row = 1, column = 1, sticky = S)
- 
+l1 = Label(text = "File: ") 
+l1.grid(row = 2, sticky = W+N+S)
 root.mainloop()
